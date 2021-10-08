7229e401792e34ad5d055591a1a76a1ad10a9abc
Will Scott <wns5@pitt.edu>
2021-10-08 16:52:42 -0400

    Create trackBuilder.py

---
+++ TrackModel/src/logic/trackBuilder.py
@@ -0,0 +1,28 @@
+'''
+Last Updated: October 8, 2021
+@author: Will Scott
+'''
+
+# Imports
+import pylightxl as xl
+
+from models.trackBlock      import trackBlock
+from models.railwayCrossing import railwayCrossing
+from models.station         import station
+from models.trackLight      import trackLight
+
+
+
+# Main Method
+def trackBuilder():
+    # Defined Variables
+    trackLayout = [];
+    
+    
+    # Opening & Reading Excel File
+
+    print("Finished Building Track Layout")
+
+
+if __name__ == '__trackBuilder__':
+    trackBuilder()
