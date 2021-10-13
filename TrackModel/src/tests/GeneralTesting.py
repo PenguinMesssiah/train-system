58301bdc5e5070eff5677dbcc8101791ea0b05ad
Will Scott <wns5@pitt.edu>
2021-10-12 22:49:11 -0400

    UI Updates

--- TrackModel/src/tests/GeneralTesting.py
+++ TrackModel/src/tests/GeneralTesting.py
@@ -21,6 +21,9 @@ def main():
         print("\nCurrent xPos: ", curObj.xPos)
         print("\nCurrent yPos: ", curObj.yPos)
         
+#  #self.browseFileSelectButton.clicked.connect(lambda:trackBuilderService.readTrackFile())
+#  self.browseFileSelectButton.clicked.connect(lambda:print("This shit is TRASH"))
+
 
 if __name__ == '__main__':
     main()
