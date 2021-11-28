60c97c67feb173d69e7ac7f5341b6e3f6485b355
Will Scott <wns5@pitt.edu>
2021-11-28 14:40:52 -0500

    Unit Testing Updates v1

--- TrackModel/src/GeneralTesting.py
+++ TrackModel/src/GeneralTesting.py
@@ -3,15 +3,19 @@ Created on October 11, 2021
 
 File for Testing Track Model Implementation
 '''
+
+import sys
 from services import trackBuilderService
 
 def main():
     
+    #Unit Test 1: Reading the Track File 7 Writing to the Database
     trackList = trackBuilderService.readTrackFile()
 
-   # print("\n\n--------Track Layout Testing---------")
-    #for curObject in trackLayout:
-    #    print('Current Object Type = ', curObject.objType);
+    #Unit Test 2: Reading Information from the Database
+
+    #Unit Test 3:
+
 
 if __name__ == '__main__':
     main()
