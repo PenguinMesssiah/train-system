fa5e3cd5e9bdcc96805289037f609c5201d14f31
theokstorldesmond <50800368+theokstorldesmond@users.noreply.github.com>
2021-11-29 10:37:04 -0500

    Add files via upload

---
+++ SWTrainController/TrainModelTest.py
@@ -0,0 +1,17 @@
+import time
+
+class TrainModel(object):
+
+    
+
+    def getAuthority(self, num):
+        return 20.0
+            
+    def getBeacon(self, num):
+        return "155th Street"
+
+    def getCommanded(self, num):
+        return 30.0
+
+    def getSpeedLimit(self, num):
+        return 40.0
