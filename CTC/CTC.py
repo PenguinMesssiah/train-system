27e1bc8f6948ad6b159492ba440c4cd877a8abac
vVPittVv <trimbureric@gmail.com>
2021-12-15 14:49:27 -0500

    dispatch

--- CTC/CTC.py
+++ CTC/CTC.py
@@ -1,22 +1,149 @@
 # File:         CTC.py
 # Maintainer:   Eric Trimbur
 # Created:      10/12/21
-# Last Updated: 10/13/21
+# Last Updated: 11/17/21
 
 # Template code from Alan D Moore, Master PyQt5 Series
 
-import sys
+import sys, os
+
 from functools import partial
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
 from PyQt5 import QtGui as qtg
 from PyQt5 import uic
 
+sys.path.append("..")
+
+from Shared.common import *
+from Shared.connections import *
+
 Ui_MainGUI, MainBaseClass = uic.loadUiType('UI/MainGUI.ui')
 Ui_InputDebug, InputDebugBaseClass = uic.loadUiType('UI/InputsDebug.ui')
 Ui_Scheduler, SchedulerBaseClass = uic.loadUiType('UI/SchedulingGUI.ui')
 
+# TODO Seperate each class into its own file
+class Block:
+
+    def __init__(self, block_number, station, crossing):
+        self.block_number = block_number
+        self.maintenance = False
+        # Station is 'none' if no station
+        self.station = station
+        # Crossing is -1 if no crossing
+        self.crossingState = crossing
+
+    def getBlockNumber(self):
+        return self.block_number
+
+    def getStation(self):
+        return self.station
+
+    def getCrossing(self):
+        return self.crossing
+    
+    def setStation(self, station):
+        self.station = station
+
+class Train:
+
+    def __init__(self, trainID, name):
+        self.trainID = trainID
+        self.name = name
+        self.current_block = -1
+        self.suggested_speed = 0
+        self.authority = [0]
+
+    def getTrainID(self):
+        return self.trainID
+
+    def getName(self):
+        return self.name
+
+    def getCurrentBlock(self):
+        return self.current_block
+
+    def setSuggestedSpeed(self, speed):
+        self.suggested_speed = speed
+
+    def setAuthority(self, authority):
+        self.authority = authority
+
+class TrainList:
+
+    def __init__(self):
+        self.trains = []
+
+    def addTrain(self, train):
+        self.trains[train.getTrainID, "train"]
+
+    def getTrain(self, trainID):
+        return self.trains[trainID]
+
+    def setSuggestedSpeed(self, speed, trainID):
+        trains[trainID].setSuggestedSpeed(speed)
+
+    def setAuthority(self, authority, trainID):
+        trains[trainID].setAuthority(authority)
+
+class ScheduleItem:
+
+    def __init__(self, trainID, time, target):
+        self.trainID = trainID
+        self.time = time
+        self.target = target
+
+    def getTrainID(self):
+        return self.trainID
+
+    def getTime(self):
+        return self.time
+
+    def getTarget(self):
+        return target
+
+class Schedule:
+
+    def __init__(self):
+        self.queue = []
+
+    def getNextStop(self, trainID):
+        for item in self.queue:
+            if item.getTrainID == trainID:
+                return item.getTime, item.getTarget
+
+    def addTrainStop(self, trainID, time, target):
+        self.queue.append(ScheduleItem(trainID, time, target))
+
+class WaysidePacket:
+    
+    def __init__(self, packetDestination, suggestedSpeed, authority):
+        self.packetDestination = packetDestination
+        self.suggestedSpeed = suggestedSpeed
+        self.authority = authority
+
+class GreenLine:
+
+    def __init__(self):
+        self.trackLayout = [];
+        # Sequential sections of track
+        for blockNum in range(149):
+            trackLoaout[blockNum] = blockNum+1
+
+
+        # Yard
+        trackLayout[0] = 63
+
+        # Switches
+        trackLayout[1] = {2, 13}
+        trackLayout[150] = 28
+        trackLayout[57] = [58, -1]
+        trackLayout[77] = [78, 101]
+        trackLayout[100] = 85;
+
+#####PYQT STUFF#####
 class SchedulerWindow(SchedulerBaseClass):
+    
     def __init__(self, parent):
         super().__init__()
 
@@ -71,27 +198,23 @@ class InputDebugWindow(InputDebugBaseClass):
         b11state = self.ui.b11o.checkState()
         self.ui.b11o.stateChanged.connect(partial(self.showOccupied, 11))
 
-        b12state = self.ui.b12o.checkState()
-        self.ui.b12o.stateChanged.connect(partial(self.showOccupied, 12))
-
-        b13state = self.ui.b13o.checkState()
-        self.ui.b13o.stateChanged.connect(partial(self.showOccupied, 13))
-
-        b14state = self.ui.b14o.checkState()
-        self.ui.b14o.stateChanged.connect(partial(self.showOccupied, 14))
-
-        b15state = self.ui.b15o.checkState()
-        self.ui.b15o.stateChanged.connect(partial(self.showOccupied, 15))
-
-
     def showOccupied(self, block):
-        targetBlock = self.parent.blocks[block-1]
+        targetBlock = self.parent.vis_blocks[block-1]
         print(targetBlock.palette().color(1).rgb())
         if targetBlock.palette().color(1).rgb() == 0xFF99c1f1:
             targetBlock.setStyleSheet("background-color: orange;")
         else:
             targetBlock.setStyleSheet("background-color: #99c1f1;")
 
+def deployTrain(block, route, trackCircut):
+   connect.train_model_dispatch_train.emit(block, route, trackCircut) 
+
+def parseCommand(inputString):
+    command = list(map(int, inputString.split()))
+
+    #dispatch
+    if command[0] == 0:
+        deployTrain(63, [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73], 0);
 
 
 class MainWindow(MainBaseClass):
@@ -104,17 +227,36 @@ class MainWindow(MainBaseClass):
 
         self.ui.actionInput_Debug.triggered.connect(self.openInputDebug);
         self.ui.actionEdit_Schedule.triggered.connect(self.openScheduling);
+        self.ui.actionDEPLOY_TRAIN.triggered.connect(deployTrain);
 
-        self.blocks = [self.ui.block1, self.ui.block2, self.ui.block3, self.ui.block4, self.ui.block5, self.ui.block6, self.ui.block7, self.ui.block8, self.ui.block9, self.ui.block10, self.ui.block11, self.ui.block12, self.ui.block13, self.ui.block14, self.ui.block15]
+        #define real blocks
+        #self.blocks = []
+        #for i in range(63,74):
+        #    self.blocks.append(Block(i, "", ""))
+
+        #self.blocks[65-63].setStation("Glenbury")
+        #self.blocks[73-63].setStation("Dormont")
+
+        #define visual blocks
+        #self.vis_blocks = [self.ui.vis_block_63, self.ui.vis_block_64, self.ui.vis_block_64, self.ui.vis_block_66, self.ui.vis_block_67, self.ui.vis_block_68, self.ui.vis_block_69, self.ui.vis_block_70, self.ui.vis_block_71, self.ui.vis_block_72, self.ui.vis_block_73]
+        
+#        for i in range(63, 74):
+#           self.vis_blocks[i-63].linkActivated.connect(self.updateTableInfo(i))
 
         self.InputDebugWin = InputDebugWindow(self)
         self.SchedulerWin = SchedulerWindow(self)
 
         self.show()
 
+
+        #self.ui.properties_table.setItem(2, 1, qtw.QTableWidgetItem(blockNum))
+
     #def showOccupied(self):
         #self.blocks[0].setStyleSheet("background-color: orange;")
         
+    #def updateTableInfo(self, blockNum):
+        #self.ui.properties_table.setItem(2, 1, qtw.QTableWidgetItem(blockNum))
+        #self.ui.properties_table.setItem(2, 3, qtw.QTableWidgetItem(self.blocks[blockNum-63].getStation()))
 
     def openInputDebug(self):
         self.InputDebugWin.show()
